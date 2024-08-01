3bdca82ed120127c095aa90e6f9ca833c5274963
Ayushagni01 <ayush.ayush24@gmail.com>
2024-08-02 01:14:56 +0530

    ReferenceQuestionofXQuestions

---
+++ Preparations/src/main/java/BasicJava/RefernceQuestionOfX.java
@@ -0,0 +1,12 @@
+package BasicJava;
+
+public class RefernceQuestionOfX {
+        public static void main(String[] args)
+        {
+            int x = 5;
+            change(x);
+            System.out.println(x);
+        }
+        public static void change(int x) { x = 10; }
+}
+

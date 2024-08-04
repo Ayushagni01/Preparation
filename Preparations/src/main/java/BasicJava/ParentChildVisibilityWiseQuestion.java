78c2a1c61fd5e81ee1089b29f869e29e7be052ad
Ayushagni01 <ayush.ayush24@gmail.com>
2024-08-05 01:01:43 +0530

    Java Basic Questions

--- Preparations/src/main/java/BasicJava/ParentChildVisibilityWiseQuestion.java
+++ Preparations/src/main/java/BasicJava/ParentChildVisibilityWiseQuestion.java
@@ -1,4 +1,20 @@
 package BasicJava;
 
+class Parent{
+    protected void getParent(){
+        System.out.println("Parent is here");
+    }
+}
+
+class Children extends Parent{
+     public  void getParent(){
+        System.out.println("This is a child class");
+    }
+}
+
 public class ParentChildVisibilityWiseQuestion {
+    public static void main(String[] args) {
+        Children children = new Children();
+        children.getParent();
+    }
 }
